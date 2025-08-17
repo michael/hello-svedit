6dd410e8c604d6766529219da1ee64f80487d95d
Michael Aufreiter <michael@letsken.com>
2025-08-17 21:50:05 +0200

    Consistent file structure.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,17 +3,15 @@
   // https://github.com/michael/svedit/blob/main/src/routes/%2Bpage.svelte
   import { Svedit } from 'svedit';
   import Overlays from '$lib/Overlays.svelte';
-  import create_demo_doc from './create_demo_doc';
+  import create_demo_doc from '$lib/create_demo_doc';
 
   const doc = create_demo_doc();
 </script>
 
-<div class="demo-wrapper">
-  <!--
-    You might want to mount a fixed toolbar <Toolbar> here. See:
-    https://github.com/michael/svedit/blob/main/src/routes/components/Toolbar.svelte
+<!--
+  You might want to mount a fixed toolbar <Toolbar> here. See:
+  https://github.com/michael/svedit/blob/main/src/routes/components/Toolbar.svelte
 
-    Or if you prefer floating tools, hack $lib/Overlays.svelte component.
-  -->
-  <Svedit {doc} editable={true} path={[doc.document_id]} {Overlays} />
-</div>
+  Or if you prefer floating tools, hack $lib/Overlays.svelte component.
+-->
+<Svedit {doc} editable={true} path={[doc.document_id]} {Overlays} />
